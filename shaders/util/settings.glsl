046fe78781529ffd5d22a0e030632bdfde8f7d03
Shadax-stack <58188800+Shadax-stack@users.noreply.github.com>
2020-12-19 14:41:07 -0800

    Removed option for hardware shadow filtering and hard shadows. Shadows will be soft and hardware filtered by default. Added shadow quality value

--- shaders/util/settings.glsl
+++ shaders/util/settings.glsl
@@ -3,46 +3,39 @@
 
 #include "formats.glsl"
 
-#define HARDWARE_SHADOW_FILTERING
-//#define SOFT_SHADOWS
-#ifdef SOFT_SHADOWS
-#ifndef HARDWARE_SHADOW_FILTERING
-#define HARDWARE_SHADOW_FILTERING
-#endif
-#endif
-
-const int shadowMapResolution = 1024;
-const float shadowDistance = 180.0f;
+const int shadowMapResolution = 2048;
+const float shadowDistance = 120.0f;
 const float ShadowDistanceFadePercentage = 0.9f;
 const float ShadowDistanceFade = shadowDistance * ShadowDistanceFadePercentage;
 const float ShadowDistanceFadeLength = shadowDistance - ShadowDistanceFade;
 const float sunPathRotation = -40.0f;
-#ifdef HARDWARE_SHADOW_FILTERING
-const bool 	shadowHardwareFiltering = true;
-#else
-const bool 	shadowHardwareFiltering = false;
-#endif
+const bool shadowHardwareFiltering = true;
 
 const int colortex1Format = RGBA16;
 
 #define SHADOW_MAP_BIAS 0.9	
 
-const float ShadowQuality = 4.0f;
+const float SoftShadowScale = 0.5f / shadowMapResolution;
+#define SHADOW_SAMPLES 2.0f
+const float ShadowSamplesPerSide = (2*(SHADOW_SAMPLES)+1);
+const float ShadowSamplesTotal = ShadowSamplesPerSide *  ShadowSamplesPerSide;
+const float ShadowQuality = 2.0f;
 const float ShadowStep = 1.0f / ShadowQuality;
-
-const float SoftShadowScale = 1.0f / shadowMapResolution;
-#define SHADOW_SAMPLES 1.0f
-const float ShadowSamplesTotal = (2*(ShadowQuality*SHADOW_SAMPLES)+1) *  (2*(ShadowQuality*SHADOW_SAMPLES)+1);
+const float ShadowQualitySamplesPerSide =  (2*(SHADOW_SAMPLES * ShadowQuality)+1);
+const float ShadowQualityArea = ShadowQualitySamplesPerSide * ShadowQualitySamplesPerSide;
+const float ShadowArea = ShadowSamplesPerSide * ShadowSamplesPerSide;
 
 const int noiseTextureResolution = 64;
 
 //#define SOFT_SHADOW_ROTATION
 
-const bool generateShadowMipmap = false;
-const float shadowIntervalSize 	= 8.0f;
+const bool generateShadowMipmap = true;
+const float shadowIntervalSize 	= 4.0f;
 
 const vec3 TorchEmitColor = vec3(0.9, 0.8, 0.6);
 
-const float ShadowDepthCompressionFactor = 0.5f;
+const float ShadowDepthCompressionFactor = 1.0f;
+
+const vec4 colortex5ClearColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
 
 #endif
