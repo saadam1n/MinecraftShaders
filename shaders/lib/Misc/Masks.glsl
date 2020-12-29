d80d892112b5f9ce76801a55fc486c8109aa3739
Shadax-stack <58188800+Shadax-stack@users.noreply.github.com>
2020-12-29 14:58:44 -0800

    Added more masking values

--- shaders/lib/Misc/Masks.glsl
+++ shaders/lib/Misc/Masks.glsl
@@ -6,10 +6,14 @@
 struct MaskStruct {
     bool Sky;
     bool Plant;
+    bool LightSource;
+    bool Sun;
 };
 
 #define SKY_BIT   1
 #define PLANT_BIT 2
+#define LIGHT_SOURCE_BIT 4
+#define SUN_BIT 8
 
 MaskStruct ConstructMaskStruct(in float id){
     MaskStruct Masks;
