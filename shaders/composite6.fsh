e07b1ba58c5501c5d9a1cb72106b64bcdbf67f7d
Shadax-stack <58188800+Shadax-stack@users.noreply.github.com>
2020-12-30 21:17:07 -0800

    Basic lens flare effect

--- shaders/composite6.fsh
+++ shaders/composite6.fsh
@@ -1,8 +1,16 @@
 #version 120
 
 #include "lib/Utility/Uniforms.glsl"
+#include "lib/Effect/LensFlare.glsl"
 
 void main() {
+    vec3 BaseColor = min(texture2D(colortex0, gl_TexCoord[0].st).rgb, texture2D(colortex1, gl_TexCoord[0].st).rgb);
+    vec3 LensFlare = ComputeLensFlare();
+    // Why not just vec3 FinalColor = BaseColor + LensFlare?
+    // It is this way so I can quickly turn on and off the effect or base color
+    vec3 FinalColor = vec3(0.0f);
+    FinalColor += BaseColor;
+    FinalColor += LensFlare;
     /* DRAWBUFFERS:7 */
-    gl_FragData[0] = min(texture2D(colortex0, gl_TexCoord[0].st), texture2D(colortex1, gl_TexCoord[0].st));
+    gl_FragData[0] = vec4(FinalColor, 1.0f);
 }
--- shaders/lib/Effect/LensFlare.glsl
+++ shaders/lib/Effect/LensFlare.glsl
@@ -1,29 +1,64 @@
 #ifndef EFFECT_LEN_FLARE_GLSL
 #define EFFECT_LEN_FLARE_GLSL 1
 
+// http://www.neocodex.us/forum/topic/126112-guide-add-lens-flare-to-seus-shader-pack/ 
+
 #include "../Utility/Uniforms.glsl"
-#include "../Geometry/Ray.glsl"
+#include "../Utility/Functions.glsl"
 
-mat4 LensFlareTransform = gbufferProjection * gbufferModelView;
-
-vec2 ComputeLensFlareLocation(in Ray lfray, in float dist){
-    vec3 Location = ExtendRay(lfray, dist);
-    vec4 ndc = LensFlareTransform * vec4(Location, 1.0f);
-    vec2 Coords = ndc.xy / ndc.w;
-    Coords = Coords * 0.5f + 0.5f;
-    return Coords;
+float CalculateLensFlareDistance(in vec2 lhs, in vec2 rhs){
+    vec2 dist = lhs - rhs;
+    dist.x *= aspectRatio;
+    return length(dist);
 }
 
-vec3 ComputeLensFlare(in vec3 sun, in vec3 direction, in vec3 cameradir){
-    return vec3(0.0f);
-    vec3 viewout = cameradir * 3.0f;
-    Ray LensFlareRay;
-    LensFlareRay.Origin = sun;
-    LensFlareRay.Direction = normalize(viewout - sun);
-    vec2 BlueLocation = ComputeLensFlareLocation(LensFlareRay, 100.0f);
-    float BlueDistance = distance(gl_TexCoord[0].st, BlueLocation);
-    float BlueStrength = 1.0f / (1.0f + 10000.0f * BlueDistance);
-    return vec3(0.0f, 0.0f, 1.0f) * BlueStrength; 
+// https://stackoverflow.com/a/5240894/12521279
+float ComputeLensFlareHexagon(in vec2 hexagon, in float size, in vec2 position){
+    const float HexagonAngle = radians(30.0f);
+    const vec2 Edge0 = vec2(0.0f, 1.0f);
+    const vec2 Edge1 = vec2(cos( HexagonAngle), sin( HexagonAngle));
+    const vec2 Edge2 = vec2(cos(-HexagonAngle), sin(-HexagonAngle));
+    position -= hexagon;
+    if(
+        dot(position, Edge0) < size || 
+        dot(position, Edge1) < size || 
+        dot(position, Edge2) < size
+    ) {
+        return 1.0f;
+    } else {
+        return 0.0f;
+    }
+}
+
+// Sildurs v1.06 extreme
+float ComputeLensFlareCircle(in vec2 circle, in float radius){
+    float dist = CalculateLensFlareDistance(gl_TexCoord[0].st, circle) / radius;
+    return exp(-dist * dist);
+}
+
+float ComputeLensFlareCircle(in vec2 light, const float blend, in float radius){
+    return ComputeLensFlareCircle(mix(light, vec2(0.5f), blend), radius);
+}
+
+vec3 ComputeLensFlare(void){
+    float SunDot = dot(normalize(sunPosition), vec3(0.0f, 0.0f, -1.0f));
+    if(SunDot < -0.6f){
+        return vec3(0.0f);
+    }
+    // Get the screen screen position
+    // Then split it up into segments between sun screen pos and center pos
+    // Then draw the lens flare stuff there
+    vec4 SunNDC = gbufferProjection * vec4(sunPosition, 1.0f);
+    vec2 SunPos = (SunNDC.xy / SunNDC.w) * 0.5f + 0.5f;
+    if(!IsInRange(SunPos, vec2(-0.1f), vec2(1.1f)) || texture2D(depthtex0, SunPos).r != 1.0f){
+        return vec3(0.0f);
+    }
+    vec3 LensFlareAccum = vec3(0.0f);
+    LensFlareAccum += ComputeLensFlareCircle(SunPos, 0.55f, 0.06f) * vec3(0.8f, 0.1f, 0.07f);
+    LensFlareAccum += ComputeLensFlareCircle(SunPos, 0.85f, 0.07f) * vec3(1.0f , 0.35f, 0.1f);
+    LensFlareAccum += ComputeLensFlareCircle(SunPos, 1.15f, 0.08f) * vec3(0.7f , 0.7f, 0.1f ) ;
+    LensFlareAccum += ComputeLensFlareCircle(SunPos, 1.45f, 0.09f) * vec3(0.15f , 0.4f, 0.9f ) ;
+    return LensFlareAccum * 0.3f * SunDot;
 }
 
 #endif
